19431adf95755000fbdfc4001695806ade7c8a86
forend600 <forend600@hotmail.com>
2026-08-05 02:51:29 +0300

    Update query to filter by specific spreadsheet name

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ async function loadAppFromGoogle() {
     let response;
     try {
         response = await gapi.client.drive.files.list({
-            q: "name=' ' and mimeType='application/vnd.google-apps.spreadsheet' and trashed=false",
+            q: "name='جمعية ال دواس' and mimeType='application/vnd.google-apps.spreadsheet' and trashed=false",
             spaces: 'drive',
         });
     } catch (err) { console.error(err); return; }
